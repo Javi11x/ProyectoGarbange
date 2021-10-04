f00782cd51772012c9a2bc17d9e89f3a3ac75436
Javi11x <84991396+Javi11x@users.noreply.github.com>
2021-10-04 14:11:33 -0500

    Add files via upload

---
+++ Garbange.Api/Entities/Denuncia.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garbange.Api.Entities
+{
+    public class Denuncia
+    {
+        
+    }
+}
